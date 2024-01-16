8ed2e6c0c7e23d739e73ea5c01cbd4b3f0c23ed0
unknown <ksmark8543@naver.com>
2024-01-16 09:21:25 +0900

    로딩테스트

--- src/app/(admin)/user/groupList/page.tsx
+++ src/app/(admin)/user/groupList/page.tsx
@@ -1,3 +1,9 @@
-export default function GroupListPage() {
+import React from "react";
+import { fakeAsyncOperation } from "~/helper/fake";
+
+const GroupListPage = async () => {
+  await fakeAsyncOperation(2000);
   return <div>GroupListPage</div>;
-}
+};
+
+export default GroupListPage;
